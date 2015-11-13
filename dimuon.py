374a1d8d90dc6391dd8a44763bc5f9a622ccaa1d
DawnPhoenix(Sophia) <hiu.wong.10@ucl.ac.uk>
2015-11-13 12:18:15 +0000

    Added particle counter.

--- dimuon.py
+++ dimuon.py
@@ -4,3 +4,8 @@ f = TFile("testdata/events.root")
 t = f.Get("events")
 n = t.GetEntries()
 print "Num. events = " + str(n)
+
+for i in xrange(n): #better than "when" so array size is not changing
+    t.GetEntry(i)
+    n2 = t.nPart
+    print "Number particles = " + str(n2)
